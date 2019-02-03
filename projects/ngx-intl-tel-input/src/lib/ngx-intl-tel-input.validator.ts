0dc44259cb0a1a5b1c45e481cc816c1ce8a6950f
dviejo <dvp123456@gmail.com>
2019-02-03 13:09:07 +0100

    Add onlyCountries attribute. Add auto country select. Add @types/google-libphonenumber.

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
@@ -4,7 +4,7 @@ import * as lpn from 'google-libphonenumber';
 export const phoneNumberValidator = (control: FormControl) => {
 	const error = { validatePhoneNumber: { valid: false } };
 
-	let number: number;
+	let number: lpn.PhoneNumber;
 	try {
 		number = lpn.PhoneNumberUtil.getInstance().parse(control.value.number, control.value.countryCode);
 	} catch (e) {
