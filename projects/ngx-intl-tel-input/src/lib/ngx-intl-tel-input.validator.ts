67bceddf2892276718868a4fa9719e756275a4c8
Aleksandr Pasevin <pasevin@gmail.com>
2020-03-11 18:11:23 +0300

    Check if element actually exists before getting attribute ‘validation’

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.validator.ts
@@ -2,29 +2,33 @@ import { FormControl } from '@angular/forms';
 import * as lpn from 'google-libphonenumber';
 
 export const phoneNumberValidator = (control: FormControl) => {
-	const isCheckValidation = document.getElementById(control.value.id || 'phone').getAttribute('validation');
-	if (isCheckValidation === 'true') {
-		const isRequired = control.errors && control.errors.required === true;
-		const error = { validatePhoneNumber: { valid: false } };
-		let number: lpn.PhoneNumber;
+	const id = control.value && control.value.id ? control.value.id : 'phone';
+	const el = document.getElementById(id) ? document.getElementById(id) : undefined;
+	if (el) {
+		const isCheckValidation = el.getAttribute('validation');
+		if (isCheckValidation === 'true') {
+			const isRequired = control.errors && control.errors.required === true;
+			const error = { validatePhoneNumber: { valid: false } };
+			let number: lpn.PhoneNumber;
 
-		try {
-			number = lpn.PhoneNumberUtil.getInstance().parse(control.value.number, control.value.countryCode);
-		} catch (e) {
-			if (isRequired === true) { return error; }
-		}
+			try {
+				number = lpn.PhoneNumberUtil.getInstance().parse(control.value.number, control.value.countryCode);
+			} catch (e) {
+				if (isRequired === true) { return error; }
+			}
 
-		if (control.value) {
-			if (!number) {
-				return error;
-			} else {
-				if (!lpn.PhoneNumberUtil.getInstance().isValidNumberForRegion(number, control.value.countryCode)) {
+			if (control.value) {
+				if (!number) {
 					return error;
+				} else {
+					if (!lpn.PhoneNumberUtil.getInstance().isValidNumberForRegion(number, control.value.countryCode)) {
+						return error;
+					}
 				}
 			}
+		} else if (isCheckValidation === 'false') {
+			control.clearValidators();
 		}
-	} else if (isCheckValidation === 'false') {
-		control.clearValidators();
 	}
 	return;
 
