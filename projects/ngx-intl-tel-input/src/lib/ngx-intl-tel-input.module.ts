bc1a843352d4441c2f970312e8242e1a2204c9c8
Tudor Maier(Home) <maier.tudor@ontegrasolutions.com>
2020-05-14 21:08:53 +0300

    Merge pull request #245 from mapiacompany/fix/ngx-bootstrap
    fix(ngx-intl-tel-input): do not use 'ngx-bootstrap' import-path

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { NgxIntlTelInputComponent } from './ngx-intl-tel-input.component';
 import { CommonModule } from '@angular/common';
-import { BsDropdownModule, TooltipModule } from 'ngx-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxIntlTelInputService } from './ngx-intl-tel-input.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
 	declarations: [NgxIntlTelInputComponent],
