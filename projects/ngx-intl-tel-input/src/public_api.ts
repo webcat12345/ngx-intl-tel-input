27125cc8c4f3f425785103065b0c4e43e8345e9c
Aleksandr Pasevin <pasevin@gmail.com>
2020-06-08 02:28:15 +0300

    Add native-element-injector.directive to public apis

--- projects/ngx-intl-tel-input/src/public_api.ts
+++ projects/ngx-intl-tel-input/src/public_api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/ngx-intl-tel-input.service';
 export * from './lib/ngx-intl-tel-input.component';
+export * from './lib/directives/native-element-injector.directive';
 export * from './lib/ngx-intl-tel-input.module';
 export * from './lib/enums/country-iso.enum';
 export * from './lib/enums/search-country-field.enum';
