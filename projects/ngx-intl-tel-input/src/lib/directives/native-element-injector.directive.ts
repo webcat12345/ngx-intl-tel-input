cfe69dd0dfea24fc450bdfc15a2c9819692d0966
Aleksandr Pasevin <pasevin@gmail.com>
2020-06-08 01:42:20 +0300

    NativeElementInjectorDirective to inject nativeElement to control

---
+++ projects/ngx-intl-tel-input/src/lib/directives/native-element-injector.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+/*
+"Property 'nativeElement' does not exist on type 'FormControl'".
+'NativeElementInjectorDirective' injects nativeElement to each control,
+so we can access it from inside validator for example.
+More about this approach and reasons for this:
+https://github.com/angular/angular/issues/18025
+https://stackoverflow.com/a/54075119/1617590
+*/
+@Directive({
+	// tslint:disable-next-line: directive-selector
+	selector: '[formControlName]',
+})
+export class NativeElementInjectorDirective implements OnInit {
+	constructor(private el: ElementRef, private control: NgControl) {}
+
+	ngOnInit() {
+		(this.control.control as any).nativeElement = this.el.nativeElement;
+	}
+}
