3c53effc1da25e1d8f66f401adfa900e1a779f64
Aleksandr Pasevin <pasevin@gmail.com>
2020-06-07 17:01:06 +0300

    Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { SearchCountryField } from 'projects/ngx-intl-tel-input/src/lib/enums/search-country-field.enum';
-import { TooltipLabel } from 'projects/ngx-intl-tel-input/src/lib/enums/tooltip-label.enum';
 import { CountryISO } from 'projects/ngx-intl-tel-input/src/lib/enums/country-iso.enum';
+import {
+	SearchCountryField
+} from 'projects/ngx-intl-tel-input/src/lib/enums/search-country-field.enum';
+import { TooltipLabel } from 'projects/ngx-intl-tel-input/src/lib/enums/tooltip-label.enum';
+
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 	separateDialCode = true;
 	SearchCountryField = SearchCountryField;
 	TooltipLabel = TooltipLabel;
 	CountryISO = CountryISO;
-	preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
+	preferredCountries: CountryISO[] = [
+		CountryISO.UnitedStates,
+		CountryISO.UnitedKingdom,
+	];
 	phoneForm = new FormGroup({
-		phone: new FormControl(undefined, [Validators.required])
+		phone: new FormControl(undefined, [Validators.required]),
 	});
 
 	changePreferredCountries() {
