ca36f291c518203305a2762948dc5f3baa4115f5
coralitriana <an4may39@gmail.com>
2023-11-04 15:40:31 -0400

    replace name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngx-intl-tel-input-app!');
+    expect(page.getTitleText()).toEqual('Welcome to ng-international-phone-field!');
   });
 });
